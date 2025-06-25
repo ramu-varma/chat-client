0d081518f58d19b87382b40e4bcdbd91d0bc92bc
ramu-varma <ramuatika@gmail.com>
2025-06-25 19:18:28 +0530

    code changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+    host: '0.0.0.0',           // ← important
+    port: process.env.PORT || 5173, // ← required for Render
+  },
+
 })
